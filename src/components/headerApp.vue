0b57e85bdee838b35c1b28c1a72435439e73d066
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-05-30 15:38:45 +0200

    scaffolding

---
+++ src/components/headerApp.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: "headerApp"
+}
+</script>
+<template></template>
+<style scoped lang="scss">
+@use '../style.scss'
+</style>
---
+++ src/components/mainSectionApp.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: "mainSectionApp"
+}
+</script>
+<template></template>
+<style scoped lang="scss">
+@use '../style.scss'
+</style>
