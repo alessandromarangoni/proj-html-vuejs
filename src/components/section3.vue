6fb1e1fdacfebf25afb4c1afcbaa3dfa2a649a4c
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-05-31 19:42:25 +0200

    section 3 last microsection (no style)

--- src/components/section3.vue
+++ src/components/section3.vue
@@ -4,10 +4,12 @@ import costumersImg2 from '../assetS/12679.jpg'
 import costumersImg3 from '../assets/oliver-ragfelt-488196-2.jpg'
 import articleImg1 from '../assets/startup-business-people-and-strategy-board-PAJ3P9K-1390x1042.jpg'
 import articleImg2 from '../assets/business-people-working-together-on-project-and-5FHSKBL-1390.jpg'
+import imgLetsTalk from '../assets/startup-business-people-and-strategy-board-PAJ3P9K-1390x1042.jpg'
 export default {
     name: "section3",
     data() {
         return {
+            imgLetsTalk,
             costumerSays: [{
                 img: costumersImg1,
                 text: "When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknown plants are noticed by when I hear the buzz of the little.",
@@ -89,10 +91,10 @@ export default {
                         </div>
                     </div>
 
-                    <div class="d-flex justify-content-between">
+                    <div class="d-flex justify-content-between mb-5">
                         <template v-for="item in this.articles">
-                            <div
-                                class=" m-3 custom_container d-flex bg-primary position-relative flex-column align-items-end">
+                            <div class=" m-3 custom_container d-flex bg-primary position-relative flex-column
+                                                                        align-items-end mb-5">
                                 <img :src="item.img" alt="" class="img-fluid">
                                 <div class="text_container bg-secondary position-absolute">
                                     <div>
@@ -105,6 +107,15 @@ export default {
                             </div>
                         </template>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid pre-footer mt-5">
+            <div class="row d-flex justify-content-center mt-5">
+                <div class="col-6 mt-5 p-5 text-center">
+                    <h3>LET'S TALK ABOUT WORK</h3>
+                    <p>a wanderful serenity has taken possession Far far away. behind the word mountain</p>
+                    <button>button</button>
                 </div>
             </div>
         </div>
@@ -147,5 +158,10 @@ export default {
         bottom: 0;
         transform: translate(0, 50%);
     }
+}
+
+.pre-footer {
+    background-image: url('../assets/business-people-working-together-on-project-and-B3MZ4TX.jpg');
+    background-size: cover;
 }
 </style>
