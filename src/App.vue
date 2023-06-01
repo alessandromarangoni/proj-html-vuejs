2ff1e2bf122f8ae073db44c0186051b23f792004
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-06-01 14:43:45 +0200

    footer + sticky elements on footer no style

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,36 @@ export default {
   <headerApp />
   <mainSectionApp />
   <footerApp />
+  <div class="message_icon_container d-flex justify-content-between align-items-center">
+    <div class="return_up d-flex justify-content-center align-items-center"><a href="#container">f</a></div>
+    <div class="message_icon d-flex justify-content-center align-items-center">
+      ciao
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.message_icon_container {
+  height: 100px;
+  width: 100px;
+  color: white;
+  position: sticky;
+  margin: -5.5rem 1rem;
+  bottom: 0;
+  left: 100%;
+
+  .message_icon {
+    height: 50%;
+    width: 50%;
+    border-radius: 50%;
+    background-color: aqua;
+  }
+
+  .return_up {
+    height: 40%;
+    width: 40%;
+    border-radius: 50%;
+    background-color: rgb(26, 255, 0);
+  }
+}
+</style>
--- src/components/footerApp.vue
+++ src/components/footerApp.vue
@@ -1,9 +1,90 @@
 <script>
+import logo from '../assets/logotype-2-139x23.png'
 export default {
-    name: "footerApp"
+    name: "footerApp",
+    data() {
+        return {
+            logo,
+            footerLink: [{
+                name: "Home",
+                link: "/"
+            }, {
+                name: "about",
+                link: "/"
+            }, {
+                name: "services",
+                link: "/"
+            }, {
+                name: "projects",
+                link: "/"
+            }, {
+                name: "blog",
+                link: "/"
+            }, {
+                name: "contacts",
+                link: "/"
+            }],
+            sociaLink: [{
+                icons: 'fa-instagram',
+                link: 'www.twitter.com'
+            }, {
+                icons: 'fa-linkedin',
+                link: 'www.linkedin.com'
+            }, {
+                icons: ' fa-facebook',
+                link: 'www.facebook.com'
+            }, {
+                icons: 'fa-twitter',
+                link: 'www.twitter.com'
+            }],
+        }
+    }
 }
 </script>
-<template></template>
+<template>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col ">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div><img :src="this.logo" alt=""></div>
+                        <div>
+                            <ul class="d-flex">
+                                <li v-for="item in footerLink" class="p-2"><a :href="item.link">{{ item.name }}</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <template v-for="items in sociaLink">
+                                <span> <a href=""> <i class="fa-brands " :class="'' + items.icons"></i> </a></span>
+                            </template>
+                        </div>
+                    </div>
+                    <div class="footer_line mb-3"></div>
+                    <div class="d-flex justify-content-between align-items-end">
+                        <div>
+                            <span class="d-block"><small><a href="">Term of use</a></small></span>
+                            <span><small><a href="">Privacy Enviromental policy</a></small></span>
+                        </div>
+                        <div><span><small><a href="">Copyright © 2020 Phlox Consulting. All Rights
+                                        Reserved.</a></small></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
 <style scoped lang="scss">
-@use '../style.scss'
+li {
+    list-style: none;
+
+    a {
+        text-decoration: none;
+    }
+}
+
+.footer_line {
+    height: 1px;
+    border: 1px solid black;
+}
 </style>
